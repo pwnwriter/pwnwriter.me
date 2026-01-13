70128923f82b792f5432773b9eadf73d46e938c9
pwnwriter <hi@pwnwriter.me>
2026-01-13 06:09:42 -0500

    feat(photos): use seprate ts file for the photos location

---
+++ src/pages/photos/photos.ts
@@ -0,0 +1,57 @@
+export type Photo = {
+  src: string;
+  alt: string;
+  date: string;
+};
+
+
+export const photos: Photo[] = [
+
+
+  {
+    src: "/images/photos/2025/IMG_3735.JPG",
+    alt: "Corolla",
+    date: "2026-01-03",
+  },
+  {
+    src: "/images/photos/2025/IMG_6296.JPG",
+    alt: "Ski day",
+    date: "2025-12-23",
+  },
+  {
+    src: "/images/photos/2025/IMG_6296.JPG",
+    alt: "Ski day",
+    date: "2025-12-23",
+  },
+  {
+    src: "/images/photos/2025/IMG_6296.JPG",
+    alt: "Ski day",
+    date: "2025-12-23",
+  },
+  {
+    src: "/images/photos/2025/IMG_6296.JPG",
+    alt: "Ski day",
+    date: "2025-12-23",
+  },
+  {
+    src: "/images/photos/2025/IMG_6296.JPG",
+    alt: "Ski day",
+    date: "2025-12-23",
+  },
+  {
+    src: "/images/photos/2025/IMG_3735.JPG",
+    alt: "Corolla",
+    date: "2026-01-03",
+  },
+  {
+    src: "/images/photos/2025/IMG_3735.JPG",
+    alt: "Corolla",
+    date: "2026-01-03",
+  },
+  {
+    src: "/images/photos/2025/IMG_3735.JPG",
+    alt: "Corolla",
+    date: "2026-01-03",
+  },
+
+];
